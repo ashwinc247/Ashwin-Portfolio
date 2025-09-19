d7e21c773584c15dc45c8705292dffb979fdfb7b
ashwinc247 <ashwinc247@gmail.com.com>
2025-09-19 16:43:07 +0530

    Navbar mobile check

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-const { color } = require("motion");
+
 
 module.exports = {
+  darkMode: 'class',
   theme: {
     extend: {
       color: {
-        'brand-gradient': "linear-gradient(to right, #9333ea, #ec4899, #f59e0b)",
+       
       },
     },
   },
