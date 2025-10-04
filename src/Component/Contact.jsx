242ba9aab9aa7b9645a6a8400cef0d3351896036
ashwinc247 <ashwinc247@gmail.com.com>
2025-10-04 15:55:18 +0530

    checking...3

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -9,6 +9,8 @@ import { AnimatedGroup } from "../../components/motion-primitives/animated-group
 
 export const Contact = (sueccess, failed) => {
 
+    const googleMapUrl = "https://maps.app.goo.gl/H54uKLSeCroMUAAo6";
+
     const public_key = "466WlVcxs7r4-4vRL";
     const service_id = "service_ec4wu0w";
     const template_id = "template_qok2rcp";
@@ -17,7 +19,7 @@ export const Contact = (sueccess, failed) => {
     const form = useRef();
     const [status, setStatus] = useState("");
 
-    const sendEmail = (e) => {
+     const  sendEmail = (e) => {
         e.preventDefault();
         setStatus("Sending...");
 
@@ -88,21 +90,21 @@ export const Contact = (sueccess, failed) => {
                     <div className="p-7 bg-PureWhite dark:bg-[#41455adc]  shadow-xl rounded-2xl mb-5">
                         <h1 className="text-2xl font-bold mt-3">Contact Information</h1>
                         <div className="mt-5">
-                            <div className="m-2 flex cursor-pointer select-none" onClick={()=>{ window.location.href = "mailto:ashwinc247@gmail.com"}}>
+                            <div className="m-2 flex cursor-pointer select-none" onClick={()=>{ window.location.href = "mailto:ashwinc247@gmail.com?subject=Portfolio Inquiry&body=Hello Ashwin,"}}>
                                 <svg className="h-12 w-12 m-2 p-3 bg-[linear-gradient(135deg,#3b82f6,#9333ea)] rounded-xl" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#FFFFFF"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" /></svg>
                                 <div className="flex  flex-col justify-center">
                                     <p className="text-xs">Email</p>
                                     <p>ashwinc247@gmail.com</p>
                                 </div>
                             </div>
-                            <div className="m-2 flex cursor-pointer select-none">
+                            <div className="m-2 flex cursor-pointer select-none" onClick={()=>( window.location.href = "tel:+916369072025")}>
                                 <svg className="h-12 w-12 m-2 p-3 bg-[linear-gradient(135deg,#3b82f6,#9333ea)] rounded-xl" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#FFFFFF"><path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z" /></svg>
                                 <div className="flex  flex-col justify-center">
                                     <p className="text-xs">Phone</p>
                                     <p>+91 6369072025</p>
                                 </div>
                             </div>
-                            <div className="m-2 flex cursor-pointer select-none">
+                            <div className="m-2 flex cursor-pointer select-none" onClick={()=>( window.open(googleMapUrl, "_blank"))} >
                                 <svg className="h-12 w-12 m-2 p-3 bg-[linear-gradient(135deg,#3b82f6,#9333ea)] rounded-xl " xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#FFFFFF"><path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" /></svg>
                                 <div className="flex  flex-col justify-center">
                                     <p className="text-xs">Location</p>
